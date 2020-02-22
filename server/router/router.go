5f7606f10402f82263a31a7d2264577ae59a857f
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2020-02-22 11:49:43 -0600

    server prod
    use build client

--- server/router/router.go
+++ server/router/router.go
@@ -1,6 +1,8 @@
 package router
 
 import (
+	"net/http"
+
 	"../middleware"
 	"github.com/gorilla/mux"
 )
@@ -16,5 +18,9 @@ func Router() *mux.Router {
 	router.HandleFunc("/api/undoTask/{id}", middleware.UndoTask).Methods("PUT", "OPTIONS")
 	router.HandleFunc("/api/deleteTask/{id}", middleware.DeleteTask).Methods("DELETE", "OPTIONS")
 	router.HandleFunc("/api/deleteAllTask", middleware.DeleteAllTask).Methods("DELETE", "OPTIONS")
+
+	prefix := "/"
+	router.PathPrefix("/").Handler(http.StripPrefix(prefix, http.FileServer(http.Dir("../client/build/"))))
+
 	return router
 }
