cd2f8686e070cda95536e728694b1ae0f534cbc3
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2020-02-22 11:07:48 -0600

    react client
    codigo preliminar

---
+++ client/build/precache-manifest.d0aaa60f3f10f9bb43b8602b67fef931.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a67dd792fc5e36dcfddf220c064cf1db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27d2945e843f9ff97e66",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "57de7b9c7ea108e6ceec",
+    "url": "/static/js/2.96c86f7f.chunk.js"
+  },
+  {
+    "revision": "967c3aa677d48917bda81b3b2ff2693c",
+    "url": "/static/js/2.96c86f7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27d2945e843f9ff97e66",
+    "url": "/static/js/main.28dcdd9f.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
